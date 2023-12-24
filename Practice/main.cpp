4c8a5b98e2e50715bf3846e28b3bf115c4853c5b
Darius <Darius@DESKTOP-JET4L1K>
2023-12-24 16:06:36 +0200

    Done with Sort And Search Algorithms

--- Practice/main.cpp
+++ Practice/main.cpp
@@ -74,13 +74,59 @@ public:
             arr[j + 1] = key;
         }
     }
+
+    //merge sort
+    static void merge(std::vector<int>& arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        std::vector<int> leftArr(n1);
+        std::vector<int> rightArr(n2);
+
+        for (int i = 0; i < n1; i++)
+            leftArr[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            rightArr[j] = arr[middle + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+    }
+
+    static void mergeSort(std::vector<int>& arr, int left, int right) {
+        if (left < right) {
+            int middle = left + (right - left) / 2;
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+            merge(arr, left, middle, right);
+        }
+    }
+
 };
 
 int main() {
-    std::array<int, 5> arr = { 12, 11, 13, 5, 6 }; //11, 12, 5, 13
-    SearchAndSort::insertionSort(arr);
-    for (int x : arr) {
-        std::cout << x << "\n";
-    }
+    
 }
 
